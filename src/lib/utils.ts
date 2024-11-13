9d663e6f36298bd42375e868f89d9020a94f38ac
Fagner Gomes <fagner.egomes@gmail.com>
2024-11-13 18:19:20 -0300

    feat: add folder utils

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,17 @@
-import { clsx, type ClassValue } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { type ClassValue, clsx } from "clsx";
+import { twMerge } from "tailwind-merge";
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
+}
+
+export function generateOTP(numberOfDigits: number) {
+  const digits = "0123456789";
+  let OTP = "";
+  const len = digits.length;
+  for (let i = 0; i < numberOfDigits; i++) {
+    OTP += digits[Math.floor(Math.random() * len)];
+  }
+
+  return OTP;
 }
