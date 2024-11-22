0225aabc2a8518ef7cb42ecf3b57db271ea04d42
Fagner Gomes <fagner.egomes@gmail.com>
2024-11-22 08:30:13 -0300

    feat: scrollview layout sidebar

--- src/app/(private)/layout.tsx
+++ src/app/(private)/layout.tsx
@@ -4,10 +4,10 @@ import { Sidebar } from "@/components/Shared/Sidebar";
 export default function LayoutDashboard({ children }: { children: React.ReactElement }) {
   return (
     <div className="flex h-full w-full">
-      <div className="hidden h-full w-60 md:block xl:fixed xl:block">
+      <div className="hidden h-full w-60 md:fixed md:block xl:fixed xl:block">
         <Sidebar />
       </div>
-      <div className="h-full w-full xl:ml-60">
+      <div className="h-full w-full md:ml-60 xl:ml-60">
         <NavBar />
         <div className="bg-[#fafbfc] p-6 dark:bg-secondary">{children}</div>
       </div>
