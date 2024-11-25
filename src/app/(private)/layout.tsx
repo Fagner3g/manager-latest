18e5b6e893d0ef3ba1b5aef88a0255d7a98b1cf5
Fagner Gomes <fagner.egomes@gmail.com>
2024-11-25 11:42:01 -0300

    feat: ajustment padding in the workspace

--- src/app/(private)/layout.tsx
+++ src/app/(private)/layout.tsx
@@ -11,7 +11,7 @@ export default function RootLayout({ children }: { children: React.ReactElement 
           <SidebarTrigger />
           <Logo />
         </div>
-        <div className="flex flex-1 flex-col gap-4 p-4 pt-0">{children}</div>
+        <div className="flex flex-1 flex-col gap-4">{children}</div>
       </SidebarInset>
     </SidebarProvider>
   );
